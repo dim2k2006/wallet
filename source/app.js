978c9a1c330fcb3382cdffb793113908424d1bb7
Дмитрий Малофеев <dim2k2006@yandex.ru>
2017-09-24 21:20:02 +0300

    Add es6 syntax support

---
+++ source/app.js
@@ -0,0 +1,33 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.send(`<!doctype html>
+	<html>
+		<head>
+			<link rel="stylesheet" href="/style.css">
+		</head>
+		<body>
+			<h1>Hello Smolny!</h1>
+		</body>
+	</html>`);
+});
+
+app.get('/error', (req, res) => {
+	throw Error('Oops!');
+});
+
+app.get('/transfer', (req, res) => {
+	const {amount, from, to} = req.query;
+	res.json({
+		result: 'success',
+		amount,
+		from,
+		to
+	});
+});
+
+export default app;
---
+++ source/server.js
@@ -0,0 +1,5 @@
+import app from './app';
+
+app.listen(3000, () => {
+	console.log('YM Node School App listening on port 3000!');
+});
