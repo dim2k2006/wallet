c129b2272ee390369e5b4e0fc867c5d315e22ce0
Дмитрий Малофеев <dim2k2006@yandex.ru>
2017-09-27 23:59:37 +0300

    Add Koa to app

--- source/app.js
+++ source/app.js
@@ -1,17 +1,41 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import Koa from 'koa';
+import serve from 'koa-static';
 import getCardsController from './controllers/cards/get';
 import addCardController from './controllers/cards/add';
 import deleteCardController from './controllers/cards/delete';
+import ApplicationError from '../libs/applicationError';
 
-const app = express();
+const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser')();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+const app = new Koa();
 
-app.get('/cards', getCardsController);
-app.post('/cards', addCardController);
-app.delete('/cards/:id', deleteCardController);
+router.get('/cards', getCardsController);
+router.post('/cards', addCardController);
+router.delete('/cards/:id', deleteCardController);
+
+// logger
+app.use(async (ctx, next) => {
+	const start = Date.now();
+	await next();
+	const ms = Date.now() - start;
+	console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+// error handler
+app.use(async (ctx, next) => {
+	try {
+		await next();
+	} catch (err) {
+		console.log('Error detected', err);
+
+		ctx.status = err instanceof ApplicationError ? err.status : 500;
+		ctx.body = `Error [${err.message}] :(`;
+	}
+});
+
+app.use(bodyParser);
+app.use(router.routes());
+app.use(serve('./public'));
 
 export default app;
