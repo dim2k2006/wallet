ccf9a7660aa1803e308357d5fcff9f0fb9dd1fe8
Дмитрий Малофеев <dim2k2006@yandex.ru>
2017-10-01 18:23:28 +0300

    Fix code style

--- source/middleware/error/index.js
+++ source/middleware/error/index.js
@@ -4,6 +4,7 @@ export default async (ctx, next) => {
 	try {
 		await next();
 	} catch (err) {
+		// eslint-disable-next-line no-console
 		console.log('Error detected', err);
 
 		ctx.status = err instanceof ApplicationError ? err.status : 500;
--- source/middleware/logger/index.js
+++ source/middleware/logger/index.js
@@ -5,5 +5,6 @@ export default async (ctx, next) => {
 
 	const ms = Date.now() - start;
 
+	// eslint-disable-next-line
 	console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 };
