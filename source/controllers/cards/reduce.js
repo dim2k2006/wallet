d42d8ab67c6803e3cc6a60ab7ec4fe1c652397ac
Дмитрий Малофеев <dim2k2006@yandex.ru>
2017-10-09 19:48:10 +0300

    Add reduceCardController

--- source/controllers/cards/reduce.js
+++ source/controllers/cards/reduce.js
@@ -5,8 +5,16 @@
 const reduceCardController = async (ctx) => {
 	const cardId = Number(ctx.params.id);
 	const amount = Number(ctx.request.body.amount);
+	const transaction = {
+		cardId,
+		type: 'paymentMobile',
+		data: '+7(921)9999999',
+		time: new Date().toISOString(),
+		sum: amount
+	};
 
 	await ctx.CardsModel.reduce(cardId, amount);
+	await ctx.TransactionsModel.create(transaction);
 	ctx.status = 200;
 };
 
