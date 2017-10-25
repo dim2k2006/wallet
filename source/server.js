3dc00e24238420619b6f5f10bcaf4c0a8966d436
Дмитрий Малофеев <dim2k2006@yandex.ru>
2017-10-25 20:07:38 +0300

    Add https

--- source/server.js
+++ source/server.js
@@ -1,7 +1,32 @@
+import fs from 'fs';
+import http from 'http';
+import https from 'https';
+
 import app from './app';
 
 const logger = require('../libs/logger')('wallet-app');
 
-app.listen(3000, () => {
-	logger.info('YM Node School App listening on port 3000!');
-});
+function listenCallback() {
+	const {port} = this.address();
+
+	logger.info(`Application started on ${port}`);
+}
+
+const LISTEN_PORT = process.env.NODE_HTTPS ? 443 : 3000;
+
+if (process.env.NODE_HTTPS) {
+	const protocolSecrets = {
+		key: fs.readFileSync('./scripts/tls/fixtures/key.key'),
+		cert: fs.readFileSync('./scripts/tls/fixtures/cert.crt')
+	};
+
+	https
+		.createServer(protocolSecrets, app.callback())
+		.listen(LISTEN_PORT, listenCallback);
+}
+
+if (!process.env.NODE_HTTPS) {
+	http
+		.createServer(app.callback())
+		.listen(LISTEN_PORT, listenCallback);
+}
